b3b9a2eea474755236fb16496b9d76fd22bdaa53
seanaleid <seanaleid@gmail.com>
2019-10-31 15:12:35 -0400

    Adds code to be deployed on Heroku

--- client/src/utils/PrivateRoute.js
+++ client/src/utils/PrivateRoute.js
@@ -10,7 +10,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
             localStorage.getItem('token') ?(
                 <Component {...props} />
             ) : (
-                <Redirect to='/login' />
+                <Redirect to='/' />
             )
         }
         />
